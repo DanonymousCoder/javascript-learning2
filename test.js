f39c886ae017c9a025a948937ad2ca22f7390135
Ridwan <aladebaruwa@gmail.com>
2023-06-07 14:58:31 +0100

    JS

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-const test= "The revolution"
+const test= "The revolution";
+console.log (test);
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
+
+
+const sgl = 'Single quotes.';
+const dbl = "Double quotes";
+console.log(sgl);
+console.log(dbl);
+
+const nam = 'Ridwan' ;
+const greeting = `Hi, ${nam}` ;
+console.log (greeting)
+
+const string = 'I\'m a boy.' ;
+console.log (string);
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7,13);
+alert(text);
+alert(part);
